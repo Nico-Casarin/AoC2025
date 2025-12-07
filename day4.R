0ab79266c95c6449db42dc8f3fff7a23d5e8ffbb
Nico-Casarin <n.casarin@proton.me>
2025-12-07 12:35:25 +0100

    day4star1 plot gif

--- day4.R
+++ day4.R
@@ -1,6 +1,7 @@
 #library
 
 library(tidyverse)
+library(magick)
 
 # Star1
 
@@ -16,6 +17,9 @@ inst_matrix2 <- cbind(
 )
 
 counter <- 0
+nst_history <- list() 
+nst_history[[1]] <- inst_matrix2
+giro <- 0
 
 for (rows in 2:(nrow(inst_matrix2) - 1)) {
   for (columns in 2:(ncol(inst_matrix2) - 1)) {
@@ -45,10 +49,71 @@ for (rows in 2:(nrow(inst_matrix2) - 1)) {
         inst_matrix2[rows + 1, columns + 1]
       )
       print(vicini)
-      print(str_count(vicini, pattern = "@"))
-      if (str_count(vicini, pattern = "@") < 4) {
+      print(str_count(vicini, pattern = "@|x"))
+      if (str_count(vicini, pattern = "@|x") < 4) {
         counter <- counter+1
+        inst_matrix2[rows, columns] <- "x"
+        giro <- giro+1
+        nst_history[[giro]] <- inst_matrix2
       }
+      #giro <- giro+1
+      #nst_history[[giro]] <- inst_matrix2
+
     }
   }
 }
+
+## Plot
+
+library(ggplot2)
+
+plot_nst(inst_matrix2)
+
+plot_nst <- function(inst_matrix) {
+  nr <- nrow(inst_matrix)
+  nc <- ncol(inst_matrix)
+
+  df <- expand.grid(
+    row = seq_len(nr),
+    col = seq_len(nc)
+  )
+  df$val <- as.vector(inst_matrix)
+
+  df$val <- factor(df$val, levels = c(".", "@", "x", "a" ))
+
+  ggplot(df, aes(x = col, y = row, fill = val)) +
+    geom_tile(
+      aes(color = val == "c"),
+      linewidth = 0.4
+    ) +
+    scale_fill_manual(values = c(
+      "." = "white",   
+      "@" = "black",   
+      "x" = "red",   
+      "a" = "white"
+    )) +
+    scale_color_manual(values = c(
+      `TRUE`  = "yellow", # bordo celle "c"
+      `FALSE` = "grey70"
+    )) +
+    scale_y_reverse(breaks = 1:nr) +   
+    coord_fixed() +
+    theme_void() +
+    theme(legend.position = "none")
+}
+
+frames <- image_graph(width = 500, height = 500, res = 96)
+
+indices <- seq(1,length(nst_history), by=50)
+
+for (s in indices) {
+  p <- plot_nst(nst_history[[s]])
+  print(p)   
+  print(s)
+}
+-
+dev.off()   
+
+gif <- image_animate(frames, fps = 5)  
+
+image_write(gif, "nst_evolution.gif") 
